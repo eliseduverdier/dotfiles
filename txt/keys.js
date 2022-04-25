0cfdb4bd5bc0967fd60f078ae4720024630eca0a
Élise <eliseduverdier@fastmail.com>
2022-04-25 17:48:26 +0200

    feat(txt): insert 4 space instead of tabs // set selection as title

--- txt/keys.js
+++ txt/keys.js
@@ -6,16 +6,54 @@
  */
 document.addEventListener('keydown', e => {
     if (e.ctrlKey && e.key === 's') {
-        e.preventDefault();
         showNotification('Saved !')
     } else if (e.ctrlKey && e.key === 'ArrowUp') {
         switchToPreviousTab()
-        e.preventDefault()
     } else if (e.ctrlKey && e.key === 'ArrowDown') {
         switchToNextTab()
-        e.preventDefault()
     } else if (e.key === 'Tab') {
-        e.preventDefault()
-        showNotification('Tab !')
+        insertFourSpaces()
+    } else if (e.ctrlKey && e.key === 'e') {
+        frameText()
+    } else if (e.ctrlKey && e.key === '?') {
+        showShortcuts()
+    } else if (e.key === 'Escape') {
+        hideShortcuts()
     }
+    e.preventDefault()
 });
+
+
+function insertFourSpaces() {
+    currentSelection = document.getSelection()
+    inNode = currentSelection.anchorNode
+    selectionStart = currentSelection.anchorOffset
+    selectionEnd = currentSelection.focusOffset
+
+    if (inNode.nodeValue !== null && inNode.nodeType === Node.TEXT_NODE) {
+        if (currentSelection.type === 'Caret') // insert 4 space
+            newContent = inNode.nodeValue.slice(0, selectionStart) + '    ' + inNode.nodeValue.slice(selectionStart)
+        /**
+         * Indentation of block not supported yet:
+         * needs to manage multiple line selection correctly
+         */
+        // else if (currentSelection.type === 'Range') // indent selection:
+        //     newContent = '   ' + inNode.nodeValue
+
+        inNode.nodeValue = newContent
+    }
+}
+
+function frameText() {
+    currentSelection = document.getSelection()
+    inNode = currentSelection.anchorNode
+
+    if (inNode.nodeValue !== null) {
+        newContent =
+            '╔═' + '═'.repeat(inNode.nodeValue.length) + '═╗\n'
+            + '║ ' + inNode.nodeValue + ' ║\n'
+            + '╚═' + '═'.repeat(inNode.nodeValue.length) + '═╝'
+
+        inNode.nodeValue = newContent
+    }
+}
--- txt/show_info.js
+++ txt/show_info.js
@@ -1,7 +1,6 @@
 /**
  * Shows information
  */
-
 function showNotification(text) {
     let notification = document.querySelector('#notification')
     notification.innerHTML = text
@@ -9,4 +8,12 @@ function showNotification(text) {
     setTimeout(function () {
         notification.setAttribute('hidden', 'true')
     }, 2000)
+}
+
+function showShortcuts() {
+    document.querySelector('#shortcuts').removeAttribute('hidden')
+}
+
+function hideShortcuts() {
+    document.querySelector('#shortcuts').setAttribute('hidden', true)
 }
